c93512285fa56e109b87ff82c7a6fe8181db27fe
Morne-Dev <morne.dev114@gmail.com>
2023-05-18 06:47:16 +0200

    Fix: tasks into vuex store

---
+++ .history/src/store/index_20230518064136.js
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex'
+
+import user from '../store/modules/user'
+
+const store = createStore({
+    state:{
+
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    },
+    getters:{
+
+    },
+    modules: {
+        user,
+    },
+})
+
+export default store
